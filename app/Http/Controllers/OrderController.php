346a899abaf080d687f0ad58a71ca84a74104289
Kyu-u <melanchthonbutarbutar@gmail.com>
2022-06-08 20:47:54 +0700

    first commit

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+class OrderController extends Controller
+{
+    //
+}
